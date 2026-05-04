3b3c6c0a09b17012d6ccff3e5d50e810e17d31e4
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-05-04 20:53:30 +0530

    Create pattern_01_square.py

---
+++ pattern_01_square.py
@@ -0,0 +1,9 @@
+#Create a star pattern in the shape of a square
+class Solution:
+    def pattern1 (self,n):
+        for i in range(n):
+            for j in range(n):
+                print("*",end="")
+            print()
+sol=Solution()
+sol.pattern1(5)
