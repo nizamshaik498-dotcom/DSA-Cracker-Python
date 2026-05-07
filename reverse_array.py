2e11cc55d48a3357893d84a02081058e36aa7085
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-05-07 21:01:01 +0530

    Update reverse_array.py

--- reverse_array.py
+++ reverse_array.py
@@ -4,3 +4,10 @@ colours=['black','yellow','green','blue','red']
 rev_colours=colours[::-1]
 print("Original array: ",colours)
 print("Reversed array: ",rev_colours)
+
+
+#Created same but using for loop 
+reverse=[]
+for i in range(len(colours)-1,-1,-1):
+    reverse.append(colours[i])
+print("Loop reversed :",reverse)
