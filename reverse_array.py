160b3fb515b5c0460540c5d75726ea63ed5a022e
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-05-07 20:56:37 +0530

    Create reverse_array.py

---
+++ reverse_array.py
@@ -0,0 +1,6 @@
+#Created an array using list and performing reverse operation on it
+
+colours=['black','yellow','green','blue','red']
+rev_colours=colours[::-1]
+print("Original array: ",colours)
+print("Reversed array: ",rev_colours)
