a8ab33706c676529928a9c04610e775055b1317a
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-05-08 21:46:23 +0530

    Create max_min_element in array.py

---
+++ max_min_element in array.py
@@ -0,0 +1,6 @@
+#Created an array using list and finding out maximum an minimum element in it.
+
+arr1=[10,85,300,250,60,560,1500,280]
+a=print(max(arr1))
+b=print(min(arr1))
+print("The average of both maximum and minium elements: ",(max(arr1)+(min(arr1))/2))
