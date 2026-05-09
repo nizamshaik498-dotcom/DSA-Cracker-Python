38b4433fc8ab03a7b9416d6ae6e64e1b89edcc3b
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-05-09 16:18:18 +0530

    Update 2nd largest in array.py

--- 2nd largest in array.py
+++ 2nd largest in array.py
@@ -3,12 +3,16 @@
 arr1 = [10, 85, 300, 250, 60, 560, 1500, 280]
 large = -1
 second_large = -1
+third_large = -1
 for num in arr1:
     if num > large:
         second_large = large
         large = num
     elif num > second_large and num != large:
         second_large = num
+    elif num > third_large and num != second_large:
+        third_large = num
 
 print("The Gold Medalist (Largest) is:", large)
 print("The Silver Medalist (Second Largest) is:", second_large)
+print("The Bronze Medalist (Third largest) is:",third_large)
