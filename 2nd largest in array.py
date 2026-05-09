ab9547e03b70ba5c8bf4c89b9a6c1930e76d6a77
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-05-09 16:13:02 +0530

    Create 2nd largest in array.py

---
+++ 2nd largest in array.py
@@ -0,0 +1,14 @@
+#Created a program which finds out the second largest element in array.
+
+arr1 = [10, 85, 300, 250, 60, 560, 1500, 280]
+large = -1
+second_large = -1
+for num in arr1:
+    if num > large:
+        second_large = large
+        large = num
+    elif num > second_large and num != large:
+        second_large = num
+
+print("The Gold Medalist (Largest) is:", large)
+print("The Silver Medalist (Second Largest) is:", second_large)
